ae378ffc22f99221c84ea69bbaea8cecf1d0d151
HDung1510 <167043485+HDung1510@users.noreply.github.com>
2024-11-29 00:36:21 +0700

    Update serializers.py

---
+++ printing/api/serializers.py
@@ -0,0 +1,7 @@
+from printing.models import History
+from rest_framework.serializers import ModelSerializer      
+
+class HistorySerializer(ModelSerializer):
+    class Meta:
+        model = History
+        fields = '__all__'
